624075fc6afbc4a2cd01b15b31e6083de8d0ee31
IsamuNakamura <crbkrk157@gmail.com>
2023-01-25 06:54:19 +0900

    small fix

---
+++ books/世界で闘うプログラミング力を鍛える本 コーディング面接189問とその解法/task/2_linkedList/linked_list.py
@@ -0,0 +1,111 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def get_node(self, i: int) -> Node:
+        n = self.head
+        for _ in range(i):
+            n = n.next
+        return n
+
+    # O(1)
+    def insert_first(self, data):
+        new_node = Node(data)
+        self.length += 1
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    # O(N)
+    def __str__(self):
+        elems = ''
+        current_node = self.head
+        while current_node:
+            if current_node.next is None:
+                elems += str(current_node.data)
+            else:
+                elems += str(current_node.data) + ', '
+            current_node = current_node.next
+        return '[' + elems + ']'
+
+    # O(1)
+    def __len__(self):
+        return self.length
+
+    # 連結リストの要素数を数える方法
+    # O(N)
+    # def __len__(self):
+    #     node = self.head
+    #     size = 0
+    #     while node is not None:
+    #         size += 1
+    #         node = node.next
+    #     return size
+
+    # O(N)
+    def insert_last(self, data):
+        if self.head is None:
+            self.insert_first(data)
+        else:
+            new_node = Node(data)
+            self.length += 1
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+
+            current_node.next = new_node
+
+    # O(N)
+    # 指定した値を削除
+    def remove(self, data):
+        if self.head is None:
+            print('リストは空です。')
+            return
+        current_node = self.head
+        previous_node = None
+        while current_node.data != data:
+            if current_node.next is None:
+                print('削除に該当するデータがありません。')
+                return
+            previous_node = current_node
+            current_node = current_node.next
+
+        # 削除ノードが先頭
+        if previous_node is None:
+            self.head = current_node.next
+            self.length -= 1
+        else:
+            previous_node.next = current_node.next
+            self.length -= 1
+
+    # 末尾(最初)の要素を削除
+    def poll(self):
+        if self.head is None:
+            print("リストは空です。")
+            return
+        elif self.length == 1:
+            self.head = None
+            self.length -= 1
+        else:
+            current = self.head
+            previous = None
+            while current.next is not None:
+                previous = current
+                current = current.next
+            previous.next = None
+            del current
+            self.length -= 1
+
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
