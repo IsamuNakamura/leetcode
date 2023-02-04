4706b9cbed068b39d802a97e57115b23cf32231c
IsamuNakamura <crbkrk157@gmail.com>
2023-02-04 16:34:16 +0900

    add 83

---
+++ 83.remove-duplicates-from-sorted-list.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=83 lang=python3
+#
+# [83] Remove Duplicates from Sorted List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return None
+        result = ListNode(0)
+        pointer = result
+        pointer.next = ListNode(head.val)
+        pointer = pointer.next
+        head = head.next
+        while head != None:
+            if pointer.val != head.val:
+                pointer.next = ListNode(head.val)
+                pointer = pointer.next
+            head = head.next
+        return result.next
+
+        # runner = head
+        # while runner:
+        #     while runner.next and runner.next.val == runner.val:
+        #         runner.next = runner.next.next
+        #     runner = runner.next
+        # return head
+
+# @lc code=end
