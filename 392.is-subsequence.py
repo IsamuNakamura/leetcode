6b3c0b98abf8c4634bcd3f9419316de3b1ab879e
IsamuNakamura <crbkrk157@gmail.com>
2023-02-04 15:51:40 +0900

    add 392

---
+++ 392.is-subsequence.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=392 lang=python3
+#
+# [392] Is Subsequence
+#
+
+# @lc code=start
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        index_s = 0
+        index_t = 0
+        while index_s < len(s) and index_t < len(t):
+            if s[index_s] == t[index_t]:
+                index_s += 1
+            index_t += 1
+        if index_s == len(s):
+            return True
+        return False
+# @lc code=end
